87557f004f8c084ec742a750132bc24ebe1ca7bb
bhbae0406 <bbae@umich.edu>
2017-04-04 09:33:53 -0400

    starting to incorporate image processing results

--- segment.py
+++ segment.py
@@ -3,6 +3,17 @@ import os
 input_dir = os.getcwd() + "/input"
 input_files = os.listdir(input_dir)
 
-for input_file in input_files: 
-    print("./threshTest " + "./input/" + input_file + " " + input_file)
-    os.system("./threshTest " + "./input/" + input_file + " " + input_file)
+jpg_files = []
+xml_files = []
+   
+for input_file in input_files:
+   extension = output_file.rsplit('.', 1)[1]   
+   if extension == "jpg":
+      jpg_files.append(input_file)
+   else if extension == "xml":
+      xml_files.append(input_file)
+      
+for input_file in xml_files: 
+   file_name = input_file.rsplit('.', 1)[0] 
+   print("./threshTest " + "./input/" + input_file + " " + file_name)
+   os.system("./threshTest " + "./input/" + input_file + " " + file_name)
